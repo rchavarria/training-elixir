8f6d973de3c6899e8aae7d4f59e1cb5231dd1be1
Rubén Chavarría <txingo@gmail.com>
2017-07-15 15:43:01 +0200

    Implement an automatic player
    Guessed letters are chosen randomly, and that's not the best algorithm
    to select letters. It usually requires more than 20 turns to win the game.

--- pc_client/lib/pc_client/interact.ex
+++ pc_client/lib/pc_client/interact.ex
@@ -1,22 +1,56 @@
 defmodule PcClient.Interact do
 
   def start() do
-    guesses = ~w{a e i o u}
+    guesses = "abcdefghijklmnopqrstuvwxyz"
+              |> String.codepoints
+              |> Enum.shuffle
 
-    Hangman.new_game("abcdef")
+    # Hangman.new_game("abcdef")
+    Hangman.new_game()
     |> play(guesses)
   end
 
   defp play(game, []), do: game
 
   defp play(game, [ current_guess | remaining_guesses ]) do
-    hangman_game = { game, tally } = Hangman.make_move(game, current_guess)
-    IO.inspect hangman_game
+    # hangman_game = { game, tally } = Hangman.make_move(game, current_guess)
+    # IO.inspect hangman_game
+    { game, tally } = Hangman.make_move(game, current_guess)
 
-    should continue?
-        if so, play next guess
-        if not, exit?
+    # IO.inspect tally
+    continue(game, tally, remaining_guesses)
+  end
 
+  defp continue(_game, tally = %{game_state: :lost}, _guesses) do
+    IO.puts [
+      "\n",
+      "You lose",
+      "\n",
+      "Guessed letters so far: ",
+      Enum.join(tally.letters, " "),
+      "\n",
+      "You used #{Enum.count(tally.used)} letters: ",
+      Enum.join(tally.used, " "),
+      "\n",
+    ]
+  end
+
+  defp continue(_game, tally = %{game_state: :won}, _guesses) do
+    IO.puts [
+      "\n",
+      "You win",
+      "\n",
+      "The word was: ",
+      Enum.join(tally.letters, ""),
+      "\n",
+      "You used #{Enum.count(tally.used)} letters: ",
+      Enum.join(tally.used, " "),
+      "\n",
+    ]
+  end
+
+  defp continue(game, _tally, guesses) do
+    play(game, guesses)
   end
 
 end
