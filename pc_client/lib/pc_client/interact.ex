39ca42cd42ab648e28669bf91293d4f8b1a0ad18
Rubén Chavarría <txingo@gmail.com>
2017-07-15 13:48:59 +0200

    Breadcrumbs with clues about how to continue with the automatic player

--- pc_client/lib/pc_client/interact.ex
+++ pc_client/lib/pc_client/interact.ex
@@ -1,13 +1,22 @@
 defmodule PcClient.Interact do
 
   def start() do
+    guesses = ~w{a e i o u}
+
     Hangman.new_game("abcdef")
-    |> play()
+    |> play(guesses)
   end
 
-  defp play(game) do
-    hangman_game = { game, tally } = Hangman.make_move(game, "a")
+  defp play(game, []), do: game
+
+  defp play(game, [ current_guess | remaining_guesses ]) do
+    hangman_game = { game, tally } = Hangman.make_move(game, current_guess)
     IO.inspect hangman_game
+
+    should continue?
+        if so, play next guess
+        if not, exit?
+
   end
 
 end
